673b73cff07d3d4cab1ab24ada4661b7fe03483e
Nikita Gurwani <gnikita@athenahealth.com>
2021-05-04 01:00:16 +0530

    refactored and enhancement

--- event-capture/src/main/java/com/ng/event_capture/data/AnalyticsEventDatabase.kt
+++ event-capture/src/main/java/com/ng/event_capture/data/AnalyticsEventDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.RoomDatabase
 import com.ng.event_capture.data.AnalyticsEventDao
 import com.ng.event_capture.data.AnalyticsEventDaoAccess
 
-@Database(entities = [AnalyticsEventDao::class], version = 1, exportSchema = false)
+@Database(entities = [AnalyticsEventDao::class], version = 2, exportSchema = false)
 abstract class AnalyticsEventDatabase : RoomDatabase(){
     companion object {
         const val DATABASE_NAME = "analyticsEventsDatabase.db"
