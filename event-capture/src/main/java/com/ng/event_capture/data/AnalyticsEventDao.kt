5dc9753de466254fe2422e503dc2d4248abde957
Nikita Gurwani <gnikita@athenahealth.com>
2021-06-13 22:04:51 +0530

    refactored and enhancement

--- event-capture/src/main/java/com/ng/event_capture/data/AnalyticsEventDao.kt
+++ event-capture/src/main/java/com/ng/event_capture/data/AnalyticsEventDao.kt
@@ -7,7 +7,9 @@ import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "AnalyticsEventTable")
 data class AnalyticsEventDao(@ColumnInfo(name = "eventName") @SerializedName("eventName") var eventName: String = "",
-                             @PrimaryKey() @ColumnInfo(name = "timeStamp") @SerializedName("timeStamp") var timeStamp: String = " ",
-                             @ColumnInfo(name = "eventProperties") @SerializedName("eventProperties") var eventProperties: String = "") {
+                             @ColumnInfo(name = "timeStamp") @SerializedName("timeStamp") var timeStamp: String = " ",
+                             @ColumnInfo(name = "eventProperties") @SerializedName("eventProperties") var eventProperties: String = "",
+                             @PrimaryKey(autoGenerate = true) val id: Long) {
+    constructor(eventName: String, timeStamp: String, eventProperties: String) : this(eventName, timeStamp, eventProperties, 0)
 
 }
